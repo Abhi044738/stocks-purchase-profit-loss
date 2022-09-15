9428d9cb3733c8b49dbc428efe36c6cf223de371
Abhi044738 <110322117+Abhi044738@users.noreply.github.com>
2022-09-15 19:41:23 +0530

    fix :for  value negative

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
-var initialPrice = document.querySelector("#initial-price")
-var stockQuantity = document.querySelector("#stock-quantity")
-var currentPrice =document.querySelector("#current-price")
-var submitBtn= document.querySelector("#submit-btn")
-var outputBox =document.querySelector("#output-box")
+var initialPrice = document.querySelector("#initial-price");
+var stockQuantity = document.querySelector("#stock-quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#submit-btn");
+var outputBox = document.querySelector("#output-box");
 
-
-function calculateProfitAndLoss(initial,quantity,current){
-    if(initial>current){
-        var loss= (initial-current)*quantity
-        var lossPercentage= (loss/initial)*100
-        showOutput(`The loss is of ${loss} and the percentage is ${lossPercentage}%`)
-    }else if(current>initial){
-        var profit=(current-initial)*quantity
-        var profitPercentage=(profit/initial)*100
-        showOutput(`The profit is of ${profit} and the percentage is ${profitPercentage}%`)
-    }else{
-        showOutput("No pain no gain and no gain no pain")
+function calculateProfitAndLoss(initial, quantity, current) {
+  if (initial > 0 && current > 0) {
+    if (initial > current) {
+      var loss = (initial - current) * quantity;
+      var lossPercentage = (loss / initial) * 100;
+      showOutput(
+        `The loss is of ${loss} and the percentage is ${lossPercentage}%`
+      );
+    } else if (current > initial) {
+      var profit = (current - initial) * quantity;
+      var profitPercentage = (profit / initial) * 100;
+      showOutput(
+        `The profit is of ${profit} and the percentage is ${profitPercentage}%`
+      );
+    } else {
+      showOutput("No pain no gain and no gain no pain");
     }
+  } else {
+    showOutput("Plesae Enter sensible value of initial & current price");
+  }
 }
 
-
-function showOutput(message){
-    outputBox.innerHTML=message
+function showOutput(message) {
+  outputBox.innerHTML = message;
 }
 
-function submitHandler(){
-    var ip=Number(initialPrice.value);
-    var qty=Number(stockQuantity.value);
-    var curr =Number(currentPrice.value);
-    calculateProfitAndLoss(ip,qty,curr);
+function submitHandler() {
+  var ip = Number(initialPrice.value);
+  var qty = Number(stockQuantity.value);
+  var curr = Number(currentPrice.value);
+  calculateProfitAndLoss(ip, qty, curr);
 }
 
-
-submitBtn.addEventListener("click",submitHandler)
+submitBtn.addEventListener("click", submitHandler);
